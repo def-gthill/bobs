1b0de9501a1e54d7749f54018d99562f0b4bb40a
Graham Hill <gthill97@gmail.com>
2021-06-14 16:46:39 -0600

    The PandasColumnTransformer works on the test case

--- bobs/prep.py
+++ bobs/prep.py
@@ -2,7 +2,10 @@
 Data preparation and cleaning functions
 """
 
+import numpy as np
 import pandas as pd
+
+from sklearn import base, compose, pipeline
 
 
 def split_y(df, y_column):
@@ -16,7 +19,7 @@ def split_y(df, y_column):
     return df.drop(y_column, axis=1), df[[y_column]]
 
 
-class ColumnAssigner:
+class ColumnAssigner(base.TransformerMixin):
     """
     A scikit-learn transformer that converts a numpy
     array into a Pandas dataframe with the specified
@@ -25,34 +28,122 @@ class ColumnAssigner:
     but earlier steps strip the column names off
     (e.g. every single built-in transformer).
     """
-    
+
     def __init__(self, column_names):
         self.column_names = column_names
-    
+
     def fit(self, data, targets=None):
         return self
-    
+
     def transform(self, data):
         return pd.DataFrame(data, columns=self.column_names)
 
 
-class ColumnKeeper:
+class PandasColumnTransformer(base.TransformerMixin):
+    """
+    A scikit-learn transformer that acts exactly like
+    a ColumnTransformer, but returns a Pandas dataframe
+    with sensible column names.
+
+    The constructor takes the same arguments as a
+    ColumnTransformer.
+
+    The implementation comes (with slight modifications to
+    make the column names nicer) from this blog post by
+    Johannes Haupt: https://johaupt.github.io/scikit-learn/tutorial/python/data%20processing/ml%20pipeline/model%20interpretation/columnTransformer_feature_names.html
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.transformer = compose.ColumnTransformer(*args, **kwargs)
+
+    def fit(self, data, targets=None):
+        self.transformer.fit(data, targets)
+        self._column_names = self._get_column_names(self.transformer)
+        return self
+
+    def transform(self, data):
+        transformed_values = self.transformer.transform(data)
+        return pd.DataFrame(
+            transformed_values,
+            index=data.index,
+            columns=self._column_names,
+        )
+
+    @classmethod
+    def _get_column_names(cls, transformer):
+        feature_names = []
+
+        for name, sub_transformer, columns, _ in transformer._iter(fitted=True):
+            if type(sub_transformer) == pipeline.Pipeline:
+                # Recursive call on pipeline
+                _names = cls._get_column_names_pipeline(sub_transformer, columns)
+                # if pipeline has no transformer that returns names
+                if len(_names) == 0:
+                    _names = columns
+                feature_names.extend(_names)
+            else:
+                feature_names.extend(cls.get_names(transformer, sub_transformer, columns))
+
+        return feature_names
+
+    @classmethod
+    def _get_column_names_pipeline(cls, transformer, columns=None):
+        for _, name, sub_transformer in transformer._iter():
+            columns = cls.get_names(transformer, sub_transformer, columns)
+
+        return columns
+
+    @classmethod
+    def get_names(cls, transformer, trans, columns):
+        if trans == 'drop' or (
+                hasattr(columns, '__len__') and not len(columns)):
+            return []
+        if trans == 'passthrough':
+            if hasattr(transformer, '_df_columns'):
+                if ((not isinstance(columns, slice))
+                        and all(isinstance(col, str) for col in columns)):
+                    return columns
+                else:
+                    return transformer._df_columns[columns]
+            else:
+                indices = np.arange(transformer._n_features)
+                return [f'x{i}' for i in indices[columns]]
+        if not hasattr(trans, 'get_feature_names'):
+            # For transformers without a get_features_names method, use the input
+            # names to the column transformer
+            if columns is None:
+                return []
+            else:
+                return columns
+
+        if columns is None:
+            return trans.get_feature_names()
+        else:
+            feature_names = list(trans.get_feature_names())
+            for i, col in enumerate(columns):
+                for j, feature_name in enumerate(feature_names):
+                    feature_names[j] = feature_name.replace(f'x{i}', col)
+            return feature_names
+
+
+class ColumnKeeper(base.TransformerMixin):
     """
     A scikit-learn transformer that keeps only the columns
     of the dataframe with the specified names.
     """
-    
+
     def __init__(self, column_names):
         self.column_names = column_names
-    
+
+    # noinspection PyUnusedLocal
     def fit(self, data, targets=None):
         return self
-    
+
     def transform(self, data):
         return data[self.column_names]
 
 
-class ColumnDropper:
+class ColumnDropper(base.TransformerMixin):
     """
     A scikit-learn transformer that drops the columns
     of the dataframe with the specified names.
@@ -76,10 +167,11 @@ class ColumnDropper:
     dropper will keep the same set of columns
     from each.
     """
-    
+
     def __init__(self, column_names):
         self.columns_to_drop = column_names
-    
+
+    # noinspection PyUnusedLocal
     def fit(self, data, targets=None):
         columns_to_drop_set = set(self.columns_to_drop)
         self.columns_to_keep = [
@@ -87,7 +179,6 @@ class ColumnDropper:
             if column not in columns_to_drop_set
         ]
         return self
-    
+
     def transform(self, data):
         return data[self.columns_to_keep]
-
--- test_bobs/test_prep.py
+++ test_bobs/test_prep.py
@@ -34,4 +34,50 @@ class TestPrep(unittest.TestCase):
                 index=['a1', 'b2', 'c3'],
             )
         )
+    
+    def test_pandas_column_transformer(self):
+        """
+        This example comes from Johannes Haupt, in
+        the same blog post as the implementation of
+        PandasColumnTransformer.
+        """
+        from sklearn.datasets import fetch_openml
+
+        from sklearn.pipeline import Pipeline
+        from sklearn.impute import SimpleImputer
+        from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+        from sklearn.model_selection import train_test_split
+        from sklearn.linear_model import LogisticRegression
         
+        x, y = fetch_openml("titanic", version=1, as_frame=True, return_X_y=True)
+        
+        numeric_features = ['age', 'fare']
+        numeric_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='median')),
+            ('scaler', StandardScaler())]
+        )
+
+        categorical_features = ['embarked', 'sex', 'pclass']
+        categorical_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+            ('onehot', OneHotEncoder(handle_unknown='ignore'))
+        ])
+
+        preprocessor = prep.PandasColumnTransformer(
+            transformers=[
+                ('num', numeric_transformer, numeric_features),
+                ('cat', categorical_transformer, categorical_features)
+            ]
+        )
+        
+        transformed = preprocessor.fit_transform(x)
+        self.assertEqual(
+            list(transformed.columns),
+            [
+                'age', 'fare',
+                'embarked_C', 'embarked_Q', 'embarked_S', 'embarked_missing',
+                'sex_female', 'sex_male',
+                'pclass_1.0', 'pclass_2.0', 'pclass_3.0',
+            ]
+        )
